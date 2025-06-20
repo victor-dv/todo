0efb71a44f75cc6543baf7c4cab2cfaffddcb516
victor-dv <js813417@gmail.com>
2025-06-20 17:26:09 -0300

    feature - Criação dos componentes[badge, button e icon]

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -11,6 +20,24 @@ export default function App() {
           Olá mundo
         </Text>
         <Text>Olá mundo</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="bg-pink-base"/>
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate className="bg-green-base" />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant={"secondary"}>3</Badge>
+        <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>nova tarefa</Button>
       </div>
     </div>
   );
