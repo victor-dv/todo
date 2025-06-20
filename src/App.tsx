23d83e101535d2acdcca35172006d56786067226
victor-dv <js813417@gmail.com>
2025-06-20 12:34:25 -0300

    feature - Criando o componente text, e realizei o download da librarie cva

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import Text from "./components/text";
+
 export default function App() {
   return (
-    <div className="text-pink-base">
-    Olá Mundo
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo
+        </Text>
+        <Text variant="body-md-bold" className="text-green-base">
+          Olá mundo
+        </Text>
+        <Text>Olá mundo</Text>
+      </div>
     </div>
-  )
+  );
 }
-
