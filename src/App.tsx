d7a0f69005c73084fbef71ee7071086ff9ac5fa2
victor-dv <js813417@gmail.com>
2025-06-22 12:43:43 -0300

    feature - Criei o input-text

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputVariants from "./components/input-text";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <div className="grid gap-10">
       <div className="flex flex-col gap-2">
         <Text variant="body-sm-bold" className="text-pink-base">
           Olá mundo
@@ -43,9 +44,11 @@ export default function App() {
       <div className="flex gap-2">
         <ButtonIcon icon={TrashIcon} />
         <ButtonIcon icon={TrashIcon} variant={"secondary"}/>
-        <ButtonIcon icon={TrashIcon} variant={"tertiary"}/>
+        <ButtonIcon icon={TrashIcon} variant={"tertiary"}/> 
+      </div>
 
- 
+      <div>
+        <InputVariants/>
       </div>
     </div>
   );
